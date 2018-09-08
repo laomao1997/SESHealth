525547c08d5082f824dc1553879b60913b3bb034
Jacob_Willis <jacebly@gmail.com>
2018-09-08 16:25:05 +1000

    Finished basic of HeartRate
    Finished basic of heart rate. Will maybe need some editing and adjusting, but it builds succesfully.

--- app/src/main/java/five/seshealthpatient/Fragments/HeartRateFragment.java
+++ app/src/main/java/five/seshealthpatient/Fragments/HeartRateFragment.java
@@ -1,30 +1,162 @@
 package five.seshealthpatient.Fragments;
 
-
-import android.os.Bundle;
+import android.Manifest;
 import android.app.Fragment;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import butterknife.ButterKnife;
 import five.seshealthpatient.R;
 
-/**
- * A simple {@link Fragment} subclass.
- */
+import static android.app.Activity.RESULT_OK;
+
 public class HeartRateFragment extends Fragment {
 
+    EditText ageEditText;
 
-    public HeartRateFragment() {
-        // Required empty public constructor
+    LinearLayout rhrLinearLayout;
+    TextView rhrTextView;
+
+    LinearLayout thrLinearLayout;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        //TODO: Instead of hardcoding the title perhaps take the user name from somewhere?
+        // Note the use of getActivity() to reference the Activity holding this fragment
+        getActivity().setTitle("Heartrate Home");
     }
+
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_heart_rate, container, false);
+        View v = inflater.inflate(R.layout.fragment_patient_information, container, false);
+
+        // Note how we are telling butter knife to bind during the on create view method
+        ButterKnife.bind(this, v);
+
+        return v;
     }
 
+
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        //setContentView(R.layout.heartrate_measure);
+
+        setUpPermissions();
+
+        ageEditText = (EditText) getView().findViewById(R.id.et_age);
+        rhrLinearLayout = (LinearLayout) getView().findViewById(R.id.ll_restingHeartRate);
+        rhrTextView = (TextView) getView().findViewById(R.id.tv_restingHeartRate);
+        thrLinearLayout = (LinearLayout) getView().findViewById(R.id.ll_targetHeartRate);
+
+        if(savedInstanceState!=null){
+            ageEditText.setText(savedInstanceState.getString("storedAge"));
+            rhrTextView.setText(savedInstanceState.getString("storedRHR"));
+        }
+
+        rhrLinearLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getRestingHeartRate();
+            }
+        });
+        thrLinearLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calculateTargetHeartRate();
+            }
+        });
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        String currentAge = ageEditText.getText().toString();
+        outState.putString("storedAge", currentAge);
+        String currentRHR = rhrTextView.getText().toString();
+        outState.putString("storedRHR", currentRHR);
+    }
+
+    private void calculateTargetHeartRate() {
+        String age = ageEditText.getText().toString();
+        if(age.matches("")){
+            Toast.makeText(getActivity(), "Please enter your age!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        String restingHeartRate = rhrTextView.getText().toString();
+        if(restingHeartRate.matches("")){
+            Toast.makeText(getActivity(), "Please calculate your RHR first!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        Intent intentToCalculateTHR = new Intent(getActivity(), HeartRate_MainActivity.class);
+
+        intentToCalculateTHR.putExtra("age", age);
+        intentToCalculateTHR.putExtra("restingHeartRate", restingHeartRate);
+
+        startActivity(intentToCalculateTHR);
+    }
+
+    public void getRestingHeartRate(){
+        Intent intentToGetRHR = new Intent(getActivity(), HeartRate_MainActivity.class);
+        startActivityForResult(intentToGetRHR, 1);
+    }
+
+    private void setUpPermissions() {
+        if(ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                String[] permissionsWeNeed = new String[]{Manifest.permission.CAMERA};
+                requestPermissions(permissionsWeNeed, 88);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 88: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // The permission was granted!
+                    //set up whatever required the permissions
+
+                } else {
+                    Toast.makeText(getActivity(), "Permission for camera not granted. HeartBeat Monitor can't run.", Toast.LENGTH_LONG).show();
+                    getActivity().finish();
+
+                    // The permission was denied, so we can show a message why we can't run the app
+                    // and then close the app.
+                }
+            }
+            // Other permissions could go down here
+
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == 1 && resultCode == RESULT_OK){
+            String restingHeartRate = data.getStringExtra("restingHeartRate");
+            rhrTextView.setText(restingHeartRate);
+        }
+    }
 }
